3a3451622d204c1901fe51d90937129af864321a
KyllianGamer <kylleke@telenet.be>
2021-01-31 20:50:03 +0100

    init
    init

---
+++ Final_Server.py
@@ -0,0 +1,124 @@
+from simple_websocket_server import WebSocketServer, WebSocket
+import asyncio
+import websockets
+import threading
+import time
+import requests
+import os
+
+API_KEY = os.getenv('API_KEY')
+
+pages = 0
+CHECK_PROCENT = 20
+
+keywords = [
+    "] Baby Yeti",
+    "Dragon Claw",
+    "Aspect of the Dragons",
+    "Mana Flux",
+    "Storm's Helmet",
+    "Storm's Chestplate",
+    "Storm's Leggings",
+    "Storm's Boots",
+    "Necron's Helmet",
+    "Necron's Chestplate",
+    "Necron's Leggings",
+    "Necron's Boots",
+    "Superior Dragon Helmet",
+    "Superior Dragon Chestplate",
+    "Superior Dragon Leggings",
+    "Superior Dragon Boots",
+    "Adaptive Helm",
+    "Adaptive Chestplate",
+    "Adaptive Leggings",
+    "Adaptive Boots",
+    "Pigman Sword",
+    "] Parrot",
+    "Shadow Assassin Helmet",
+    "Shadow Assassin Chestplate",
+    "Shadow Assassin Leggings",
+    "Shadow Assassin Boots"
+]
+
+connections = []
+
+class Handler(threading.Thread):
+    def getName(self, uuid):
+        data = requests.get("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid).json()
+        return data["name"]
+
+    def getItems(self, keyword, PAGES):
+        items = []
+        for page in PAGES:
+            for pageItem in page:
+                if keyword in pageItem['item_name']:
+                    if 'bin' in pageItem and pageItem['bin'] == True:
+                        items.append(pageItem)  
+        return items
+
+    def CheckPrice(self, keyword, PAGES):
+        items = self.getItems(keyword, PAGES)
+        lowest_price = 9999999999
+        lowest_price_2 = 1
+        lowest_item = {}
+        prices = []
+        for item in items:
+            price = item['starting_bid']
+            prices.append(price)
+            if price < lowest_price:
+                lowest_item = item
+                lowest_price_2 = lowest_price
+                lowest_price = price
+            elif price < lowest_price_2:
+                lowest_price_2 = price
+        procent = 100-(lowest_price / lowest_price_2 * 100)
+        if procent >= CHECK_PROCENT:
+            return lowest_item
+        else:
+            return False
+
+    async def LoadData(self):
+        PAGES = []
+        pageData = requests.get('https://api.hypixel.net/skyblock/auctions?page=0&key=' + API_KEY).json()
+        PAGES.append(pageData['auctions'])
+        pages = pageData['totalPages']
+        for iPage in range(pages):
+            print(iPage)
+            PAGES.append(requests.get('https://api.hypixel.net/skyblock/auctions?page=' + str(iPage) + '&key=' + API_KEY).json()['auctions'])
+        for keyword in keywords:
+            getItem = self.CheckPrice(keyword, PAGES)
+            if getItem != False:
+                get_name = self.getName(getItem['auctioneer'])
+                if len(connections) == 0:
+                    print("No Connections")
+                    return
+                print(getItem)
+                for ws in connections:
+                    ws.send_message("ADD " + getItem['item_name'].replace(" ", "_") + " " + get_name + " " + getItem['tier'] + " " + str(getItem['starting_bid']) + " " + getItem['item_lore'].replace(" ", "_"))
+    def run(self):
+        while True:
+            if len(connections) > 0:
+                asyncio.run(self.LoadData())
+            
+            
+
+class Server(WebSocket):
+    def handle(self):
+        self.send_message(self.data)
+
+    def connected(self):
+        print(self.address, 'connected')
+        connections.append(self)
+
+    def handle_close(self):
+        print(self.address, 'closed')
+        connections.remove(self)
+
+
+handler = Handler()
+handler.start()
+server = WebSocketServer('192.168.0.149', 8080, Server)
+server.serve_forever()
+    
+
+
