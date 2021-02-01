c5cfd4bc156f2d51ee9304ec920c669c1775b7cb
KyllianGamer <kylleke@telenet.be>
2021-02-01 19:22:34 +0100

    Update Final_Server.py

--- Final_Server.py
+++ Final_Server.py
@@ -86,8 +86,8 @@ class Handler(threading.Thread):
             if iPage % 5 == 0:
                 for ws in connections:
                     ws.send_message("IDLE")
-                    print("/// Idle ///")
-                    print(iPage)
+                print("/// Idle ///")
+                print(iPage)
             #print(iPage)
             PAGES.append(requests.get('https://api.hypixel.net/skyblock/auctions?page=' + str(iPage) + '&key=' + API_KEY).json()['auctions'])
         for keyword in keywords:
