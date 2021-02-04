158f4db3def8054308af6da58352bd82bf63eb5a
KyllianGamer <kylleke@telenet.be>
2021-02-04 07:41:44 +0100

    Update Final_Server.py

--- Final_Server.py
+++ Final_Server.py
@@ -118,6 +118,7 @@ class Handler(threading.Thread):
                     return
                 print(getItem)
                 for ws in connections:
+                    print("ADD " + getItem['item_name'].replace(" ", "_") + " " + get_name + " " + getItem['tier'] + " " + str(getItem['starting_bid']) + " " + getItem['item_lore'].replace(" ", "_") + " " + getItem['pot_profit'])
                     ws.send_message("ADD " + getItem['item_name'].replace(" ", "_") + " " + get_name + " " + getItem['tier'] + " " + str(getItem['starting_bid']) + " " + getItem['item_lore'].replace(" ", "_") + " " + getItem['pot_profit'])
     def run(self):
         while True:
